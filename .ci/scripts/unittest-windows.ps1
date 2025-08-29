ccda41f5ecbe193735742d460bee87c24438260a
Gregory James Comer <gjcomer@meta.com>
2025-08-29 00:04:25 -0700

    Run Python unit test CI on Windows
    ghstack-source-id: 84dd0858318dfedc41283acde2b907563a544b7f
    ghstack-comment-id: 3226055310
    Pull-Request: https://github.com/pytorch/executorch/pull/13716

---
+++ .ci/scripts/unittest-windows.ps1
@@ -0,0 +1,33 @@
+param (
+    [string]$editable
+)
+
+Set-PSDebug -Trace 1
+$ErrorActionPreference = 'Stop'
+$PSNativeCommandUseErrorActionPreference = $true
+
+conda create --yes --quiet -n et python=3.12
+conda activate et
+
+# Activate the VS environment - this is required for Dynamo to work, as it uses MSVC.
+# There are a bunch of environment variables that it requires.
+# See https://learn.microsoft.com/en-us/cpp/build/building-on-the-command-line.
+& "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\Common7\Tools\Launch-VsDevShell.ps1" -Arch amd64
+
+if ($editable -eq 'true') {
+    install_executorch.bat --editable
+} else {
+    install_executorch.bat
+}
+if ($LASTEXITCODE -ne 0) {
+    Write-Host "Installation was unsuccessful. Exit code: $LASTEXITCODE."
+    exit $LASTEXITCODE
+}
+
+# Run pytest with coverage
+# pytest -n auto --cov=./ --cov-report=xml
+pytest --continue-on-collection-errors -vv --timeout=600 --full-trace
+if ($LASTEXITCODE -ne 0) {
+    Write-Host "Pytest invocation was unsuccessful. Exit code: $LASTEXITCODE."
+    exit $LASTEXITCODE
+}
